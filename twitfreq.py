6d74bd55a7fc94e8913477ea47423ab9cf4e8883
Matt Yetter <dmajoraddnine@gmail.com>
2012-08-19 15:43:42 -0500

    very, very close. need to format output better

--- twitfreq.py
+++ twitfreq.py
@@ -1,5 +1,4 @@
-import sys, os, urllib.request, urllib.parse, json, re
-from operator import itemgetter
+import sys, urllib.request, urllib.parse, json, re, operator
 
 if len(sys.argv) < 2:
 	#sys.exit('Usage: \'python %s [--includeRetweets] [--includeReplies] [twitterHandle]\'' % sys.argv[0]) (deal with this later)
@@ -7,55 +6,58 @@ if len(sys.argv) < 2:
 else:
 	handle = sys.argv[1]
 
+#tokenizes a tweet and adds the words to a hashtable (dict), then returns the lower tweet ID between the sentinel and the current tweet ID
+def processTweet( tweet, hash, lowestID ):
+	cleanText = re.sub( '[!?,.]', '', tweet['text'] ) #strip punctuation that would interfere with hashing
+	tokens = cleanText.split()
+	for t in tokens:
+		try:
+			hash[t] = hash[t] + 1
+		except KeyError:
+			hash[t] = 1
+	return ( min( lowestID, tweet['id'] ) )
+
 wordHash = {}
 tweetsGotten = 0
 outputFile = open( 'twitfreq-output-%s.txt' % handle, 'w' )
 
 while tweetsGotten < 1000:
-	if( tweetsGotten % 200 == 0 ):
-		print( 'Getting new tweets...(%d gotten so far)' % tweetsGotten )
+	print( 'Getting new tweets...(%d gotten so far)' % tweetsGotten )
 	
+	#build the GET parameters
 	paramDict = { 'screen_name':handle, 'count':200, 'trim_user':'true' }
-	try: #only add max_id and since_id if loop has run at least once already
-		#NOTE: subtracting 1 from oldest tweet ID only works because we are on 64-bit Python. see: https://dev.twitter.com/docs/working-with-timelines
-		paramDict['since_id'] = newestTweetProcessed
-		paramDict['max_id'] = ( oldestTweetProcessed - 1 )
+	try: #only need max_id if loop has run at least once already
+		paramDict['max_id'] = lowestTweetId
 	except NameError:
 		pass
+	
+	#perform the request
 	params = urllib.parse.urlencode( paramDict )
 	try:
 		response = urllib.request.urlopen( 'http://api.twitter.com/1/statuses/user_timeline.json?%s' % params )
 	except IOError as error:
 		print( 'HTTP Error!\n Code=%d\nMessage=%s\nHeaders=%s' % ( error.code, error.msg, error.hdrs ) )
 		sys.exit()
+	headers = dict( response.info() )
+	print( 'Request successful! Length=%s, Requests Remaining=%s' % ( headers['Content-Length'], headers['X-RateLimit-Remaining'] ) )
 	try:
 		content = response.read()
 		utfContent = content.decode( 'utf8' )
 		data = json.loads( utfContent )
 	except:
-		sys.exit( 'Uh oh!  Error loading content from twitter.  content=%s\nutfContent=%s\ndata=%s' % content, utfContent, data )
+		sys.exit( 'Uh oh!  Error parsing JSON.\ncontent=%s\nutfContent=%s\ndata=%s' % ( content, utfContent, data ) )
 	
 	#process tweets -- tokenize then hash
 	for tweet in data:
-		cleanText = re.sub( '[!?,.]', '', tweet['text'] ) #strip punctuation that would interfere with tokenization
-		tokens = cleanText.split()
-		for t in tokens:
-			try:
-				wordHash[t] = wordHash[t] + 1
-			except KeyError: #key is not present
-				wordHash[t] = 1
-		
-		#logistics
-		tweetsGotten += 1
 		try:
-			newestTweetProcessed = max( newestTweetProcessed, tweet['id'] ) #recent tweets have HIGHER numbered IDs
-		except NameError: #first run of loop
-			newestTweetProcessed = tweet['id']
-		try:
-			oldestTweetProcessed = min( oldestTweetProcessed, tweet['id'] ) #older tweets have LOWER numbered IDs
-		except NameError: #first run of loop
-			oldestTweetProcessed = tweet['id']
+			if( tweet['id'] != lowestTweetId ): #don't process the same tweet twice. see: https://dev.twitter.com/docs/working-with-timelines
+				lowestTweetId = processTweet( tweet, wordHash, lowestTweetId )
+				tweetsGotten += 1
+		except NameError:
+			lowestTweetId = processTweet( tweet, wordHash, tweet['id'] )
+			tweetsGotten += 1
 			
 #sort the dict and output results
-for w in ( sorted( wordHash, key=itemgetter( 1 ), reverse=True ) ):
-	outputFile.write( '%s\n' % w[0] )
+print( wordHash.popitem() )
+for w in ( sorted( wordHash.items(), key=operator.itemgetter( 1 ), reverse=True ) ):
+	outputFile.write( '%s\n' % w[0].encode( 'ascii', 'ignore' ) )
